208783b8973b8a636f8e7a76b31e855fa088e97c
unknown <debbiemeer30@gmail.com>
2025-02-06 23:53:06 +0800

    updated home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -102,7 +102,7 @@ export default function Home() {
         </div>
         
         {/* Decorative Elements */}
-        <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-0">
+        <div className="absolute top-0 left-0 w-full h-full overflow-hidden z-[-1]">
           <div className="absolute top-0 left-1/4 w-96 h-96 bg-red-500 rounded-full mix-blend-multiply filter blur-3xl opacity-10 animate-blob"></div>
           <div className="absolute top-0 right-1/4 w-96 h-96 bg-yellow-500 rounded-full mix-blend-multiply filter blur-3xl opacity-10 animate-blob animation-delay-2000"></div>
           <div className="absolute bottom-0 left-1/3 w-96 h-96 bg-green-500 rounded-full mix-blend-multiply filter blur-3xl opacity-10 animate-blob animation-delay-4000"></div>
